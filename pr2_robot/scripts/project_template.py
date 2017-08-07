ef27f100bf4b76c3972a9675b6aaab0c364480a1
lisaljl <lisa.lau.jl@gmail.com>
2017-08-07 23:41:09 +0100

    Perform object recognition on three different tabletop configurations

--- pr2_robot/scripts/project_template.py
+++ pr2_robot/scripts/project_template.py
@@ -17,6 +17,7 @@ from sensor_stick.pcl_helper import *
 import rospy
 import tf
 from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
 from std_msgs.msg import Float64
 from std_msgs.msg import Int32
 from std_msgs.msg import String
@@ -62,7 +63,7 @@ def pcl_callback(pcl_msg):
 
     # Voxel Grid Downsampling
     vox = cloud_filtered.make_voxel_grid_filter()
-    LEAF_SIZE = 0.01
+    LEAF_SIZE = 0.007
     vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
     cloud_filtered = vox.filter()
 
@@ -71,7 +72,7 @@ def pcl_callback(pcl_msg):
     filter_axis = 'z'
     passthrough.set_filter_field_name (filter_axis)
     axis_min = 0.6
-    axis_max = 0.8
+    axis_max = 0.9
     passthrough.set_filter_limits (axis_min, axis_max)
     cloud_filtered = passthrough.filter()
 
@@ -99,9 +100,9 @@ def pcl_callback(pcl_msg):
     white_cloud = XYZRGB_to_XYZ(cloud_objects)
     tree = white_cloud.make_kdtree()
     ec = white_cloud.make_EuclideanClusterExtraction()
-    ec.set_ClusterTolerance(0.08)
+    ec.set_ClusterTolerance(0.05)
     ec.set_MinClusterSize(100)
-    ec.set_MaxClusterSize(1000)
+    ec.set_MaxClusterSize(1500)
     ec.set_SearchMethod(tree)
     cluster_indices = ec.Extract()
 
@@ -132,97 +133,145 @@ def pcl_callback(pcl_msg):
 # Exercise-3 TODOs:
 
     # Classify the clusters! (loop through each detected cluster one at a time)
-    # detected_objects_labels = []
-    # detected_objects = []
+    detected_objects_labels = []
+    detected_objects = []
 
-    # for index, pts_list in enumerate(cluster_indices):
+    for index, pts_list in enumerate(cluster_indices):
 
-    #     # Grab the points for the cluster
-    #     cloud_objects_cluster = cloud_objects.extract(pts_list)
-    #     ros_cluster = pcl_to_ros(cloud_objects_cluster)
-    #     # Compute the associated feature vector
-    #     chists = compute_color_histograms(ros_cluster, using_hsv=True)
-    #     normals = get_normals(ros_cluster)
-    #     nhists = compute_normal_histograms(normals)
-    #     feature = np.concatenate((chists, nhists))
+        # Grab the points for the cluster
+        cloud_objects_cluster = cloud_objects.extract(pts_list)
+        ros_cluster = pcl_to_ros(cloud_objects_cluster)
+        # Compute the associated feature vector
+        chists = compute_color_histograms(ros_cluster, using_hsv=True)
+        normals = get_normals(ros_cluster)
+        nhists = compute_normal_histograms(normals)
+        feature = np.concatenate((chists, nhists))
         
-    #     # Make the prediction
-    #     prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
-    #     label = encoder.inverse_transform(prediction)[0]
-    #     detected_objects_labels.append(label)
+        # Make the prediction
+        prediction = clf.predict(scaler.transform(feature.reshape(1,-1)))
+        label = encoder.inverse_transform(prediction)[0]
+        detected_objects_labels.append(label)
 
-    #     # Publish a label into RViz
-    #     label_pos = list(white_cloud[pts_list[0]])
-    #     label_pos[2] += .4
-    #     object_markers_pub.publish(make_label(label,label_pos, index))
+        # Publish a label into RViz
+        label_pos = list(white_cloud[pts_list[0]])
+        label_pos[2] += .4
+        object_markers_pub.publish(make_label(label,label_pos, index))
 
-    #     # Add the detected object to the list of detected objects.
-    #     do = DetectedObject()
-    #     do.label = label
-    #     do.cloud = ros_cluster
-    #     detected_objects.append(do)
+        # Add the detected object to the list of detected objects.
+        do = DetectedObject()
+        do.label = label
+        do.cloud = ros_cluster
+        detected_objects.append(do)
 
-    # # Publish the list of detected objects
-    # rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
+    # Publish the list of detected objects
+    rospy.loginfo('Detected {} objects: {}'.format(len(detected_objects_labels), detected_objects_labels))
 
-    # detected_objects_pub.publish(detected_objects)
+    detected_objects_pub.publish(detected_objects)
+
+    labels = []
+    centroids = [] # to be list of tuples (x, y, z)
+    for object in detected_objects:
+        labels.append(object.label)
+        points_arr = ros_to_pcl(object.cloud).to_array()
+        centroids.append(np.mean(points_arr, axis=0)[:3])
+
+    detected_objects_list = dict(zip(labels, centroids))
 
     # Suggested location for where to invoke your pr2_mover() function within pcl_callback()
     # Could add some logic to determine whether or not your object detections are robust
     # before calling pr2_mover()
-    # try:
-    #     pr2_mover(detected_objects_list)
-    # except rospy.ROSInterruptException:
-    #     pass
+    try:
+        pr2_mover(detected_objects_list)
+    except rospy.ROSInterruptException:
+        pass
 
 # function to load parameters and request PickPlace service
-def pr2_mover(object_list):
+def pr2_mover(detected_objects_list):
 
-    # TODO: Initialize variables
+    # Initialize variables
+    test_scene_num = Int32()
+    object_name = String()
+    arm_name = String()
+    pick_pose = Pose()
+    pick_pose_point = Point()
+    place_pose_point = Point()
+    place_pose = Pose()
+    dropbox_pos_dict = {}
+    dropbox_arm_dict = {}
+    dict_list = []
+    pick_list = 3
 
-    # TODO: Get/Read parameters
+    # Get/Read parameters
+    object_list_param = rospy.get_param('/object_list')
+    dropbox_param = rospy.get_param('/dropbox')
 
-    # TODO: Parse parameters into individual variables
+    # Parse parameters into individual variables
+    for dropbox in dropbox_param:
+        dropbox_pos_dict[dropbox['group']] = dropbox['position']
+        dropbox_arm_dict[dropbox['group']] = dropbox['name']
 
     # TODO: Rotate PR2 in place to capture side tables for the collision map
 
-    # TODO: Loop through the pick list
+    # Loop through the pick list
+    for object in object_list_param:
 
-        # TODO: Get the PointCloud for a given object and obtain it's centroid
+        test_scene_num.data = pick_list
+        group = object['group']
+        object_name.data = object['name']
+        pos = detected_objects_list.get(object['name'])
+        if pos is None:
+            pick_pose_point.x = 0
+            pick_pose_point.y = 0
+            pick_pose_point.z = 0
+        else:
+            pick_pose_point.x = np.asscalar(pos[0])
+            pick_pose_point.y = np.asscalar(pos[1])
+            pick_pose_point.z = np.asscalar(pos[2])
+        pick_pose.position = pick_pose_point
 
-        # TODO: Create 'place_pose' for the object
+        # Create 'place_pose' for the object
+        place_pose_point.x = dropbox_pos_dict[group][0]
+        place_pose_point.y = dropbox_pos_dict[group][1]
+        place_pose_point.z = dropbox_pos_dict[group][2]
+        place_pose.position = place_pose_point
 
-        # TODO: Assign the arm to be used for pick_place
+        # Assign the arm to be used for pick_place
+        arm_name.data = dropbox_arm_dict[group]
 
-        # TODO: Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+        # Create a list of dictionaries (made with make_yaml_dict()) for later output to yaml format
+        # Populate various ROS messages
+        yaml_dict = make_yaml_dict(test_scene_num, arm_name, object_name, pick_pose, place_pose)
+        dict_list.append(yaml_dict)
 
-        # Wait for 'pick_place_routine' service to come up
-        rospy.wait_for_service('pick_place_routine')
 
-        try:
-            pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
+    # Wait for 'pick_place_routine' service to come up
+    rospy.wait_for_service('pick_place_routine')
 
-            # TODO: Insert your message variables to be sent as a service request
-            resp = pick_place_routine(TEST_SCENE_NUM, OBJECT_NAME, WHICH_ARM, PICK_POSE, PLACE_POSE)
+    try:
+        pick_place_routine = rospy.ServiceProxy('pick_place_routine', PickPlace)
 
-            print ("Response: ",resp.success)
+        # TODO: Insert your message variables to be sent as a service request
+        resp = pick_place_routine(test_scene_num, object_name, arm_name, pick_pose, place_pose)
 
-        except rospy.ServiceException, e:
-            print "Service call failed: %s"%e
+        print ("Response: ",resp.success)
 
-    # TODO: Output your request parameters into output yaml file
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+    # Output your request parameters into output yaml file
+    send_to_yaml("output_" + str(pick_list) + ".yaml", dict_list)
 
 
 
 if __name__ == '__main__':
 
-    # TODO: ROS node initialization
+    # ROS node initialization
     rospy.init_node('clustering', anonymous=True)
 
-    # TODO: Create Subscribers
+    # Create Subscribers
     pcl_sub = rospy.Subscriber("/pr2/world/points", pc2.PointCloud2, pcl_callback, queue_size=1)
 
-    # TODO: Create Publishers    
+    # Create Publishers    
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue_size=1)
     pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue_size=1)
     pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue_size=1)
@@ -230,11 +279,17 @@ if __name__ == '__main__':
     detected_objects_pub = rospy.Publisher("/detected_objects", DetectedObjectsArray, queue_size=1)
 
 
-    # TODO: Load Model From disk
+    # Load Model From disk
+    model = pickle.load(open('model.sav', 'rb'))
+    clf = model['classifier']
+    encoder = LabelEncoder()
+    encoder.classes_ = model['classes']
+    scaler = model['scaler']
 
     # Initialize color_list
     get_color_list.color_list = []
 
-    # TODO: Spin while node is not shutdown
+
+    # Spin while node is not shutdown
     while not rospy.is_shutdown():
         rospy.spin()
